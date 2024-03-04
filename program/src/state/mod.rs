032cd472c5b945ba7822320c286d36bb92565f10
febo <febo@tensor.so>
2024-03-04 15:59:38 +0000

    Add assert decode helper

--- program/src/state/mod.rs
+++ program/src/state/mod.rs
@@ -8,5 +8,28 @@ pub use full_merkle_proof::*;
 pub use mint_proof::*;
 pub use whitelist::*;
 
+use anchor_lang::{prelude::*, AccountDeserialize};
+use solana_program::{account_info::AccountInfo, pubkey::Pubkey};
+use vipers::throw_err;
+
+use crate::error::ErrorCode;
+
 /// Constant for zero array.
 pub const ZERO_ARRAY: [u8; 32] = [0; 32];
+
+#[inline(never)]
+pub fn assert_decode_whitelist(whitelist_info: &AccountInfo) -> Result<Whitelist> {
+    let mut data: &[u8] = &whitelist_info.try_borrow_data()?;
+    let whitelist: Whitelist = AccountDeserialize::try_deserialize(&mut data)?;
+
+    let (key, _) = Pubkey::find_program_address(&[&whitelist.uuid], &crate::ID);
+    if key != *whitelist_info.key {
+        throw_err!(ErrorCode::BadWhitelist);
+    }
+    // Check account owner (redundant because of find_program_address above, but why not).
+    if *whitelist_info.owner != crate::ID {
+        throw_err!(ErrorCode::BadWhitelist);
+    }
+
+    Ok(whitelist)
+}
