171ddb8756ff5bc1fc9d66925bbb081eab257f57
leant <155785958+leantOnSol@users.noreply.github.com>
2024-05-15 18:31:51 +0200

    examples dir + small helper func (#16)
    * examples v3 + added helper func
    
    * smol fix
    
    * lint
    
    * fix whitelistPda name

--- clients/js/src/helpers.ts
+++ clients/js/src/helpers.ts
@@ -3,6 +3,7 @@ import {
   getAddressDecoder,
   getAddressEncoder,
 } from '@solana/addresses';
+import { WhitelistSeeds, findWhitelistPda } from './generated';
 
 export type NumberData = number[] | Uint8Array | Buffer;
 
@@ -15,3 +16,12 @@ export const fromAddress = (address: Address): Uint8Array => {
   const encoder = getAddressEncoder();
   return encoder.encode(address);
 };
+
+export async function getWhitelistFromCollId(collId: string): Promise<Address> {
+  const uuid = Buffer.from(collId.split('-').join(''));
+  const whitelistSeeds: WhitelistSeeds = {
+    uuid: uuid,
+  };
+  const [whitelistPda] = await findWhitelistPda(whitelistSeeds);
+  return whitelistPda;
+}
