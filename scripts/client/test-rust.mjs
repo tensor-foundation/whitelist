e078113ac8302fb4dcb195a2f9695a27b7055a88
Samuel Vanderwaal <samv@tensor.so>
2024-07-29 11:27:16 -0800

    fix things

--- scripts/client/test-rust.mjs
+++ scripts/client/test-rust.mjs
@@ -1,12 +1,12 @@
 #!/usr/bin/env zx
-import 'zx/globals';
-import { workingDirectory } from '../utils.mjs';
+import "zx/globals";
+import { workingDirectory } from "../utils.mjs";
 
 // Run the tests.
-cd(path.join(workingDirectory, 'clients', 'rust'));
-const hasSolfmt = await which('solfmt', { nothrow: true });
+cd(path.join(workingDirectory, "clients", "rust"));
+const hasSolfmt = await which("solfmt", { nothrow: true });
 if (hasSolfmt) {
-  await $`cargo test-sbf ${argv._} 2>&1 | solfmt`;
+  await $`BPF_OUT_DIR='../../target/deploy' cargo test-sbf ${argv._} 2>&1 | solfmt`;
 } else {
-  await $`cargo test-sbf ${argv._}`;
+  await $`BPF_OUT_DIR='../../target/deploy' cargo test-sbf ${argv._}`;
 }
